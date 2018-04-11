1ae79a0fc17e12325b995d64050ca7bc25991660
codeherlife <codeherlife@gmail.com>
2018-04-10 17:05:45 -0700

    First commit

---
+++ app/src/main/java/com/codeherlife/newfoodorderclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codeherlife.newfoodorderclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
