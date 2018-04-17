00e363a131ecf622c4286e0cb12d7deb679d5aa4
codeherlife <codeherlife@gmail.com>
2018-04-16 21:22:29 -0700

    added databse reference and edit email

--- app/src/main/java/com/codeherlife/newfoodorderclient/MainActivity.java
+++ app/src/main/java/com/codeherlife/newfoodorderclient/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private EditText email, pass;
-
+    private DatabaseReference mDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email= (EditText) findViewById(R.id.editEmail);
+
     }
 }
